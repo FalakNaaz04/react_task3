370d2fa3b81c6fe7e02f11650508c07567e1437f
unknown <naazfalak5119@gmail.com>
2023-10-22 15:29:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import ColorPicker from "./components/ColorPicker";
+
+export default function App() {
+  return (
+    <div>
+      <ColorPicker />
+    </div>
+  );
+}
---
+++ src/components/ColorPicker.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { SketchPicker } from "react-color";
+import "../components/styles.css";
+
+function ColorPicker() {
+  const [color, setColor] = useState("lightblue");
+  const [hidden, setHidden] = useState(false);
+  const pickerStyle = {
+    default: {
+      picker: {
+        position: "absolute",
+        bottom: "30px",
+        left: "100px",
+      },
+    },
+  };
+  return (
+    <div className="App" style={{ background: color }}>
+      <div className="container">
+        {hidden && (
+          <SketchPicker
+            styles={pickerStyle}
+            color={color}
+            onChange={(updatedColor) => setColor(updatedColor.hex)}
+          />
+        )}
+
+        <button onClick={() => setHidden(!hidden)}>
+          {hidden ? "Close Color Picker" : "Open Color Picker"}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ColorPicker;
